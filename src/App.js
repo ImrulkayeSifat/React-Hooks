aa971b3424046b119d910253a995d0e7d449d924
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-08 23:10:08 +0600

    complete useDeferredValue hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
-import React, { useState,useTransition } from 'react';
+import React, { useState } from 'react';
+import ListL from './ListL';
 
 function App() {
-  const [isPending,startTransition] = useTransition();
-  const [input,setInput] = useState('');
-  const [list,setList] = useState([]);
 
-  const LIST_SIZE = 20000;
+  const [input,setInput] = useState('');
 
   function handleChange(e){
     setInput(e.target.value);
-    startTransition(()=>{
-      const l =[];
-      for(let i=0;i<LIST_SIZE;i++)
-      {
-        l.push(e.target.value);
-      }
-      setList(l);
-    })
-
   }
   return (
     <>
       <input type="text" value={input} onChange={handleChange} />
-      {isPending ? "loading....":list.map((item,index)=>{
-        return <div key={index}>{item}</div>
-      })}
+      <ListL input={input}/>
     </>
   );
 }
