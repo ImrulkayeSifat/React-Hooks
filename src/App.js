055c4e909b0359ce13ca63c125a9fc5789c36e03
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-08 20:02:27 +0600

    useMemo return function value,on the other hand useCallback return entire funcion

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import './App.css';
-import React, { useState,useMemo } from 'react';
-
+import React, { useState,useCallback } from 'react';
+import List from './List';
 
 function App() {
   
   const [number,setNumber] = useState(0);
   const [dark,setDark] = useState(false);
 
-  const doubleNumber = useMemo(()=>{
-    return slowFunction(number);
+  const getItems = useCallback(() => {
+    return [number,number+1,number+2]
   },[number])
-  const themeStyes = {
-    backgroundColor: dark ? 'black':'white',
-    color : dark ? 'white': 'black'
+
+  const theme = {
+    backgroundColor: dark ? '#333':'#FFF',
+    color : dark ? '#FFF': '#333'
   }
 
   return (
-    <>
+    <div style={theme}>
       <input type="number" value={number} onChange = {e=>setNumber(parseInt(e.target.value))}/>
       <button onClick={()=>setDark(prevDark => !prevDark)}>change theme</button>
-      <div style={themeStyes}>{doubleNumber}</div>
-    </>
+      <List getItems={getItems} />
+    </div>
   );
 }
 
-function slowFunction(num){
-  console.log("calling slow function");
-  for(let i=0;i<=1000;i++){}
-  return num*2;
-}
 
 export default App;
