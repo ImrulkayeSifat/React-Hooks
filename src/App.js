21272bad8d812f51dc1679126b61c4eff9b293a6
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-08 22:34:56 +0600

    set priority for state manage

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import './App.css';
-import React, { useReducer, useState } from 'react';
-import Todo from './Todo';
+import React, { useState,useTransition } from 'react';
 
-export const ACTIONS = {
-  ADD_TODO: 'add-todo',
-  TOGGLE_TODO:'toggle-todo',
-  DELETE_TODO:'delete-todo'
-}
-
-function reducer(todos,action){
-  // eslint-disable-next-line default-case
-  switch(action.type){
-    case ACTIONS.ADD_TODO:
-      return [...todos,newTodo(action.payload.name)]
-    case ACTIONS.TOGGLE_TODO:
-      return todos.map(todo=>{
-        if(todo.id === action.payload.id){
-          return {...todo,complete:!todo.complete}
-        }
-        return todo;
-      })
-    case ACTIONS.DELETE_TODO:
-      return todos.filter(todo=>todo.id !== action.payload.id)
-    default:
-      return todos;      
-  }
-}
-
-function newTodo(name){
-  return {id:Date.now(),name:name,complete:false}
-}
 function App() {
-  const [todos,dispatch] = useReducer(reducer,[]);
-  const [name,setName] = useState('');
+  const [isPending,startTransition] = useTransition();
+  const [input,setInput] = useState('');
+  const [list,setList] = useState([]);
 
-  function handleSubmit(e){
-    e.preventDefault();
-    dispatch({type:ACTIONS.ADD_TODO,payload:{name:name}})
-    setName('');
+  const LIST_SIZE = 20000;
+
+  function handleChange(e){
+    setInput(e.target.value);
+    startTransition(()=>{
+      const l =[];
+      for(let i=0;i<LIST_SIZE;i++)
+      {
+        l.push(e.target.value);
+      }
+      setList(l);
+    })
+
   }
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
-      </form>
-      {todos.map(todo=>{
-        return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending ? "loading....":list.map((item,index)=>{
+        return <div key={index}>{item}</div>
       })}
     </>
   );
