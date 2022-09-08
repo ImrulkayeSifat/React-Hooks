e1198816e458873b125a49aa8a1eda6150d0c58a
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-08 18:25:52 +0600

    complete useRef

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import React from 'react';
-import FunctionContextComponent from './FunctionContextComponent';
-import {ThemeProvider} from './ThemeContext';
+import React, { useState,useRef, useEffect } from 'react';
+
 
 function App() {
+  const [name,setName] = useState('')
+  const renderCount = useRef(0);
+  const inputRef = useRef();
+
+  useEffect(()=>{
+    renderCount.current = renderCount.current +1;
+  })
+
+  function focus(){
+    inputRef.current.focus()
+  }
+
   return (
     <>
-      <ThemeProvider>
-        <FunctionContextComponent />
-      </ThemeProvider>
+      <input ref={inputRef} value={name} onChange={e=>setName(e.target.value)} />
+      <div>My name is {name}</div>
+      <button onClick={focus}>focus</button>
+      <div>I renderd {renderCount.current} times</div>
     </>
   );
 }
