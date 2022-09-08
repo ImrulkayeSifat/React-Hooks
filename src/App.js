f65a162fb90ab3856abe480cdc815c9cd6194c81
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-08 15:04:49 +0600

    complete useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import React,{useState} from 'react';
+
+function countInitial(){
+  console.log('run function')
+  return 4;
+}
 
 function App() {
+  let [count,setCount]=useState(()=>countInitial());
+
+  // let [state,setState] = useState({count:4,theme:'blue'});
+  // const count = state.count;
+  // const theme = state.theme;
+
+  // function decrementCount(){
+  //   setCount(prevState=>{
+  //     return {...prevState,count:prevState.count-1}
+  //   });
+  // }
+
+  function decrementCount(){
+    setCount(prevCount=>prevCount-1);
+  }
+
+  function incrementCount(){
+    setCount(prevCount=>prevCount+1);
+  }
   return (
     <>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </>
   );
 }
