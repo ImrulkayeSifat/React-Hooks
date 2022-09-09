e1b595ca2f863259c6d3eb02d8c105592f40484e
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-09 22:40:25 +0600

    create useTimeOut hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
 import ToggleComponent from './1-useToggle/ToggleComponent';
-
+import TimeoutComponent from './2-useTimeout/TimeoutComponent';
 
 function App() {
 
   return (
     <>
       {/* <ToggleComponent /> */}
+      <TimeoutComponent />
     </>
   );
 }
