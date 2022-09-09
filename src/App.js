01655087e552c7487aef9620729631f563c49742
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-09 22:47:08 +0600

    implement debounce hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react';
 import ToggleComponent from './1-useToggle/ToggleComponent';
 import TimeoutComponent from './2-useTimeout/TimeoutComponent';
+import DebounceComponent from './3-useDebounce/DeounceComponent';
 
 function App() {
 
   return (
     <>
       {/* <ToggleComponent /> */}
-      <TimeoutComponent />
+      {/* <TimeoutComponent /> */}
+      <DebounceComponent />
     </>
   );
 }
