4776860a7301390bdedc7b63bac5d5d9c03f963e
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-09 22:54:11 +0600

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ToggleComponent from './1-useToggle/ToggleComponent';
 import TimeoutComponent from './2-useTimeout/TimeoutComponent';
 import DebounceComponent from './3-useDebounce/DeounceComponent';
-
+import UpdateEffectComponent from './4-useUpdateEffect/UpdateEffectComponent';
 function App() {
 
   return (
@@ -11,6 +11,7 @@ function App() {
       {/* <ToggleComponent /> */}
       {/* <TimeoutComponent /> */}
       {/* <DebounceComponent /> */}
+      {/* <UpdateEffectComponent /> */}
     </>
   );
 }
