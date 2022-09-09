6f6de425e78837adcefa6ac816e20a62644d0dad
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-09 23:01:51 +0600

    create useArray hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ToggleComponent from './1-useToggle/ToggleComponent';
 import TimeoutComponent from './2-useTimeout/TimeoutComponent';
 import DebounceComponent from './3-useDebounce/DeounceComponent';
 import UpdateEffectComponent from './4-useUpdateEffect/UpdateEffectComponent';
+import ArrayComponent from './5-useArray/ArrayComponent';
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
       {/* <TimeoutComponent /> */}
       {/* <DebounceComponent /> */}
       {/* <UpdateEffectComponent /> */}
+      {/* <ArrayComponent /> */}
     </>
   );
 }
