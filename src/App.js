aab18b033bcf50e7f350aef629332a7601f53326
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-09 22:27:47 +0600

    custom useToggle Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React from 'react';
-import useLocalStorage from './useLocalStorage';
-import useUpdateLogger from './useUpdateLogger';
+import ToggleComponent from './1-useToggle/ToggleComponent';
+
 
 function App() {
-  const [name,setName] = useLocalStorage('name','g');
-  useUpdateLogger(name);
+
   return (
     <>
-      <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
+      {/* <ToggleComponent /> */}
     </>
   );
 }
