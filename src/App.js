3df44ba3b004316e1faa408f378c42f399eafcdc
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-09 21:42:08 +0600

    check whether input value change or not!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
 import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
 
 function App() {
   const [name,setName] = useLocalStorage('name','g');
-
+  useUpdateLogger(name);
   return (
     <>
       <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
