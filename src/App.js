411c7acdaab7651fb80066417cc952923c97061b
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-10 21:36:08 +0600

    create useScript hook

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import StateWithHistoryComponent from './7-useStateWithHistory /StateWithHistory
 import StorageComponent from './8-useStorage/StorageComponent';
 import AsyncComponent from './9-useAsync/AsyncComponent';
 import FetchComponent from './10-useFetch/ FetchComponent';
+import ScriptComponent from './11-useScript/ScriptComponent';
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
       {/* <StorageComponent /> */}
       {/* <AsyncComponent /> */}
       {/* <FetchComponent /> */}
+      {/* <ScriptComponent /> */}
     </>
   );
 }
