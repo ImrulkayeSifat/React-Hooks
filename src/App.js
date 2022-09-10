c1d95a84d323c0793c8cae8fe7ec2dccfd69e67c
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-10 12:51:46 +0600

    #

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       {/* <DebounceComponent /> */}
       {/* <UpdateEffectComponent /> */}
       {/* <ArrayComponent /> */}
-      <PreviousComponent />
+      {/* <PreviousComponent /> */}
     </>
   );
 }
