44cda65abd0d3dcd0315ff96d2ed5a25f94e4a54
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-10 21:45:49 +0600

    #

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       {/* <AsyncComponent /> */}
       {/* <FetchComponent /> */}
       {/* <ScriptComponent /> */}
-      <DeepCompareEffectComponent />
+      {/* <DeepCompareEffectComponent /> */}
     </>
   );
 }
