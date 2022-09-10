8a48c3e90321cd5322c373f43a16d4f45b87dfe1
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-10 13:57:32 +0600

    #

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       {/* <ArrayComponent /> */}
       {/* <PreviousComponent /> */}
       {/* <StateWithHistoryComponent /> */}
-      <StorageComponent />
+      {/* <StorageComponent /> */}
     </>
   );
 }
