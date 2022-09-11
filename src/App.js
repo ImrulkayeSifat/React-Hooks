2154be7244c8ff11689e52f6c1da21795371bedb
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-11 21:14:52 +0600

    create useTranslation hook

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
       {/* <DarkModeComponent /> */}
       {/* <CopyToClipboardComponent /> */}
       {/* <CookieComponent /> */}
-      <TranslationComponent />
+      {/* <TranslationComponent /> */}
     </>
   );
 }
